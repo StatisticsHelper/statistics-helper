ea07e30f4d5e26b274629c5d9d274cbce5b640fa
sharafeddeen <ad17asha@uwcad.it>
2023-01-05 14:38:15 -0600

    implemented navigation menu: needs styling

---
+++ website-code/index.js
@@ -0,0 +1,12 @@
+const navIcon = document.getElementById('nav-icon');
+const navMenu = document.getElementById('nav-menu');
+
+navMenu.style.display = 'none';
+
+navIcon.addEventListener('click', () => {
+  if (navMenu.style.display === 'block') {
+    navMenu.style.display = 'none';
+  } else {
+    navMenu.style.display = 'block';
+  }
+});
