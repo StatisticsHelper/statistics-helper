7fd33bd1ae6fdde278d13a389d30d910b2213610
sharafeddeen <ad17asha@uwcad.it>
2023-07-07 12:29:54 -0500

    fixed issue #14
    now items JSON list auto updates every 24 hours from Zotero

--- website-code/pyzotero/test.sh
+++ website-code/pyzotero/test.sh
@@ -3,19 +3,49 @@
 # Set variables
 URL="https://api.zotero.org/groups/2547147/items"
 API_KEY="W2NMV5HBdkanpmNaPrsF4G7b"
+# you can't make it more than 100 - hard limit!
 ITEMS_PER_PAGE=100
-ITEMS_TOTAL=$(curl -s -H "Zotero-API-Key: $API_KEY" "${URL}?limit=1" | awk -F':' '{print $2}' | tr -d ',}')
-PAGES=$((ITEMS_TOTAL / ITEMS_PER_PAGE + 1))
+PAGES=30
 
-# Loop through each page and download items
-for ((i=0; i<$PAGES; i++))
-do
-    curl -s -H "Zotero-API-Key: $API_KEY" "${URL}?start=${i}00&limit=${ITEMS_PER_PAGE}" >> items.json
+# Create an empty array to store all the requests
+all_requests=()
+
+# Loop through requests and store them in the array
+for ((PAGE=0; PAGE < PAGES; PAGE++)); do
+
+    # request the items and store the response
+    response=$(curl -s -H "Zotero-API-Key: $API_KEY" "$URL?limit=$ITEMS_PER_PAGE&start=$(( (PAGE) * ITEMS_PER_PAGE ))")
+
+    # Check if the response is empty
+    if [ ${#response[@]} -eq 0 ]  || [ "${response[*]}" = "[]" ]; then
+        #do nothing
+        :
+    else
+        # modify response (remove [ ] and add ,)
+        response="${response#\[}"
+        response="${response%\]}"
+        response="$response,"
+        # add the response to the all_requests array
+        all_requests+=("${response[@]}")
+    fi
 done
 
-# Check if items.json is not empty
-if [ -s items.json ]; then
-    echo "Items downloaded successfully."
-else
-    echo "No items were downloaded."
-fi
+# Print all_requests
+
+# 1. get size (different from PAGES because some responses are empty)
+all_requests_size=${#all_requests[@]}
+
+# 2. remove `,` from the end of the array
+all_requests[all_requests_size - 1]="${all_requests[all_requests_size - 1]%\,}"
+
+# 3. empty the output file
+file_path="items.json"
+> "$file_path"
+
+# 4. echo the array
+    # each element in the array is a response (containing <=100 items)
+echo "[" >> "$file_path"
+for ((id=0; id < all_requests_size; id++)); do
+    echo ${all_requests[id]} >> "$file_path"
+done
+echo "]" >> "$file_path"
